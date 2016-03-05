9b2a0eccb512961809183c5fb29ff23c8776e575
noguhiro <g007c1280@gmail.com>
2016-03-05 11:21:28 +0900

    first commit.

--- tests/HashMatchTest.php
+++ tests/HashMatchTest.php
@@ -7,12 +7,62 @@ use Rmtram\SimilarImage\Strategy\HashStrategy;
 
 class HashMatchTest extends AbstractTestCase
 {
+
+    public function testP001()
+    {
+        $this->compareTestImage('p001', 2);
+    }
+
+    public function testP002()
+    {
+        $this->compareTestImage('p002', 2);
+    }
+
+    public function testP003()
+    {
+        $this->compareTestImage('p003', 2);
+    }
+
+    public function testP004()
+    {
+        $this->compareTestImage('p004', 2);
+    }
+
+    public function testP005()
+    {
+        $this->compareTestImage('p005', 2);
+    }
+
+    public function testP006()
+    {
+        $this->compareTestImage('p006', 2);
+    }
+
     public function testP007()
     {
-        $images = $this->loadClassImage('p007');
+        $this->compareTestImage('p007', 3);
+    }
+
+    public function testP008()
+    {
+        $this->compareTestImage('p008', 2);
+    }
+
+    public function testP009()
+    {
+        $this->compareTestImage('p009', 2);
+    }
+
+    /**
+     * @param string  $cluster
+     * @param int $actual
+     */
+    private function compareTestImage($cluster, $actual)
+    {
+        $images = $this->loadClassImage($cluster);
         $base = reset($images);
         $similar = new SimilarImage(new HashStrategy());
         $ret = $similar->tolerance(75)->run($base, $images);
-        var_dump($ret);
+        $this->assertEquals(count($ret), $actual);
     }
 }
